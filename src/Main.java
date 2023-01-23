a1a41b8b496f9fd2c65e451db03e648041d6fb25
zotin <zotinalexander@gmail.com>
2023-01-23 12:25:51 +0100

    homework done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,81 @@
+import driver.Driver;
+import driver.DriverB;
+import driver.DriverD;
+import driver.DriverС;
+import exception.DiagnosticFaildException;
+import transport.Mehanic;
+import transport.TransportType;
+
+import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        DriverB driverB = new DriverB("Водитель категории B", true, 1);
+        DriverС driverC = new DriverС("Водитель категории C", true, 3);
+        DriverD driverD = new DriverD("Водитель категории D", true, 2);
+
+
+        Car Car = new Car("BMW", "3", 1.6, driverB);
+        Bus Bus = new Bus("Iveco", "1345", 2.5, driverD);
+        Truck Truck = new Truck("Volvo", "Go", 3.0, driverC);
+
+
+        try {
+            System.out.println(Car.diagnostics());
+        } catch (DiagnosticFaildException e) {
+            e.printStackTrace();
+        }
+
+        List<Driver> drivers = new ArrayList<>();
+        drivers.add(driverB);
+        drivers.add(driverC);
+        drivers.add(driverD);
+
+
+        Mehanic mehanic1 = new Mehanic("Sodov Alex", "Michelin", TransportType.ALL);
+        Mehanic mehanic2 = new Mehanic("Sidorov mihail", "Michelin", TransportType.ALL);
+        Mehanic mehanic3 = new Mehanic("Nodo Alexey", "Michelin", TransportType.ALL);
+
+        Car.addMechanic(mehanic1);
+        Car.addMechanic(mehanic1);
+        Car.addMechanic(mehanic2);
+
+        Bus.addMechanic(mehanic3);
+
+        Truck.addMechanic(mehanic1);
+        Truck.addMechanic(mehanic2);
+        Truck.addMechanic(mehanic3);
+
+        List<Transport<?>> racers = new ArrayList<>();
+
+        racers.add(Car);
+        racers.add(Bus);
+        racers.add(Truck);
+
+        Map<Transport<?>, Mehanic> transportMechanicMap = new HashMap<>();
+
+        for (Transport<?> racer : racers) {
+            Set <Mehanic> mehanics = racer.getMehanics();
+
+            while (mehanics.iterator().hasNext()) {
+                transportMechanicMap.put(racer, mehanics.iterator().next());
+            }
+        }
+
+        for (Transport<?> transport : racers) {
+            System.out.println(transport + " " + transport.getDriver() + " " + transport.getMehanics());
+        }
+
+    }
+    private static void printInfo(Transport<?> transport) {
+        transport.maxSpeed();
+        transport.pitStop();
+        transport.theBestTimeLap();
+
+        System.out.println(" " + transport.getDriver() + " управляет автомобилем "
+                + transport.getBrand() + " " + transport.getModel() + " обьемом " + transport.getEngineVolume() + " и будет учавствовать в заезде");
+
     }
 }
---
+++ src/Transport.java
@@ -0,0 +1,105 @@
+import driver.Driver;
+import exception.DiagnosticFaildException;
+import transport.Mehanic;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public abstract class Transport<T extends Driver> implements Competing, com.sun.jdi.connect.Transport {
+
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private final T driver;
+    private final Set<Mehanic> mehanics = new HashSet<>();
+
+
+    private static final String DEFAULT_VALUE = "default";
+    private static final double DEFAULT_ENGINE_VOLUME = 1.5;
+
+
+    public Transport(String brand,
+                     String model,
+                     double engineVolume,
+                     T driver) {
+
+        if (brand == null || brand.isBlank()) {
+            this.brand = DEFAULT_VALUE;
+        } else {
+            this.brand = brand;
+        }
+        if (model == null || model.isBlank()) {
+            this.model = DEFAULT_VALUE;
+        } else {
+            this.model = brand;
+        }
+        setEngineVolume(engineVolume);
+
+
+        this.driver = driver;
+
+    }
+
+    public abstract void startMove();
+
+    public abstract void finischMove();
+
+    public abstract void printType();
+
+    public abstract boolean diagnostics() throws DiagnosticFaildException;
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        if (engineVolume <= 0) {
+            engineVolume = DEFAULT_ENGINE_VOLUME;
+        }
+        this.engineVolume = engineVolume;
+    }
+
+    public T getDriver() {
+        return driver;
+    }
+
+    public Set<Mehanic> getMehanics() {
+        return mehanics;
+    }
+
+    public void addMechanic(Mehanic mehanic) {
+        mehanics.add(mehanic);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Transport{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume +
+                '}';
+    }
+
+    public abstract void refill();
+
+    public Transport(T driver) {
+        this.driver = driver;
+    }
+
+
+
+}
+
+
